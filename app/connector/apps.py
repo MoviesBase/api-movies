edc7816cec5fe8a5ad0ebefb3c1a7f500279f704
Tiko Kobiashvili <tiikokobiashvili@gmail.com>
2024-03-04 16:11:52 +0100

    fixed signals

--- app/connector/apps.py
+++ app/connector/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class ConnectorConfig(AppConfig):
     name = 'connector'
 
-    # def ready(self):
-    #     import connector.signals  # Import signals module # noqa
+    def ready(self):
+        import connector.signals  # Import signals module # noqa
--- app/connector/signals.py
+++ app/connector/signals.py
@@ -1,5 +1,6 @@
 # signals.py
 
+from django.conf import settings
 from django.core.management import call_command
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
@@ -10,4 +11,8 @@ def run_fetch_movie_data(sender, **kwargs):
     """
     Function to run a custom command after migrations are applied.
     """
-    call_command('fetch_movie_data')
+    if settings.TEST:
+        # Don't execute the signal during tests
+        return
+    else:
+        call_command('fetch_movie_data')
